d6ae07f04dfac01bcdfeaff9bceee0a5614e5798
Thomas Aloi <Tom Aloi@DESKTOP-RSAFK29>
2022-11-02 17:15:32 -0400

    Removed forgot password on loging and made sign up button link to register page

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -96,13 +96,8 @@ export default function LoginScreen() {
                             Sign In
                         </Button>
                         <Grid container>
-                            <Grid item xs>
-                                <Link href="#" variant="body2">
-                                    Forgot password?
-                                </Link>
-                            </Grid>
                             <Grid item>
-                                <Link href="#" variant="body2">
+                                <Link href="/register" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
