e17b6e74339f8a59ad4156d76a2b657d5d98205d
Thomas Aloi <Tom Aloi@DESKTOP-RSAFK29>
2022-11-02 21:32:18 -0400

    deleting a playlist visually updates

--- client/src/components/MUIErrorModal.js
+++ client/src/components/MUIErrorModal.js
@@ -7,17 +7,6 @@ import { Alert } from '@mui/material';
 import AlertTitle from '@mui/material/AlertTitle';
 import Button from '@mui/material/Button';
 
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-};
 
 export default function MUIErrorModal() {
     const { auth } = useContext(AuthContext);
